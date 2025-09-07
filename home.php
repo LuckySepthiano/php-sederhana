0e27f559d3d6d782ee0b1abc50ce01bc77a787d5
LuckySepthiano <olagebek@gmail.com>
2025-09-08 00:18:26 +0700

    home.php

---
+++ home.php
@@ -0,0 +1,13 @@
+<?php
+// home.php - halaman utama sederhana
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple PHP Home</title>
+</head>
+<body>
+    <h1>Welcome to Simple PHP Home Page</h1>
+    <p>This is a basic PHP page deployed on Hugging Face Spaces using Docker.</p>
+</body>
+</html>
